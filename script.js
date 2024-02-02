127722512f77a753d9c51e086ece532f2b4fb7e1
chintriago <chrisintriago94@gmail.com>
2024-02-01 20:50:56 -0800

    medium fixes edits and restructure of code

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// document.addEventListener('DOMContentLoaded', function () {
+const tipForm = document.querySelector('#tip-form');
+const bill = document.querySelector('#bill');
+const buttonsArray = document.querySelectorAll('.btn-js');
+const buttonsList = document.getElementsByClassName('btn-js');
+const customButton = document.querySelector("#custom-button");
+const tipSpan = document.querySelector('#tip');
+const totalSpan = document.querySelector('#total');
+const resetButton = document.querySelector('#reset-button');
+
 let billValue = 0;
 let btnValue = 0;
 let customValue = 0;
@@ -7,17 +15,57 @@ let result = 0;
 let tipPerson = 0;
 let totalPerson = 0;
 
+// makes all button elements toggle a background color.
+// Only one button can change at a time and when clicked.
+// If clicked anywhere else it does not disappear.
+buttonsArray.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        // Remove 'active' and 'font-color classes from all buttons
+        event.preventDefault();
+        buttonsArray.forEach(function (btn) {
+            btn.classList.remove('active');
+            btn.classList.remove('font-color');
+            // prevents default behavior
+            // for all buttons
+            event.preventDefault();
+        });
+
+        // Add 'active' and 'font-color' classes to the inititally clicked button
+        button.classList.add('active');
+        button.classList.add('font-color');
+    });
+});
+
+function removeButtonStyles() {
+    for (let i = 0; i < buttonsList.length; i++) {
+        buttonsList[i].classList.remove('active');
+        buttonsList[i].classList.remove('font-color');
+    }
+};
+
+// when custom button is clicked all other buttons active styling gets removed
+// learned to not put () after the name of your own function inside
+// the event listener solution found below
+// https://stackoverflow.com/questions/70920592/why-does-a-javascript-event-listener-function-need-to-be-wrapped-in-function-tag
+customButton.addEventListener('click', removeButtonStyles);
+
+// when reset button is clicked form is reset
+resetButton.addEventListener('click', function () {
+    removeButtonStyles();
+    tipForm.reset();
+});
+
+// CODE BELOW IS MAIN FUNCTIONALITY OF THE TIP CALCULATOR APP
+// IT NEEDS TO BE LAST IN FILE TO WORK PROPERLY
+
 // calculates cost times percentage then divides by number of people
 // only if all variable have a numerical value higher than 0
 // found solution below
 // https://stackoverflow.com/questions/68308011/looking-for-a-way-to-calculate-2-of-3-input-fields-in-realtime-using-javascript
-const reset = document.querySelector('#reset');
-const tipSpan = document.querySelector('#tip');
-const totalSpan = document.querySelector('#total');
 
 function tipCalculator(cost, percent, number) {
     if (cost > 0 && percent > 0 && number > 0) {
-        reset.classList.remove('tinted');
+        resetButton.classList.remove('tinted');
         tipPerson = (cost * percent) / number;
         tipPerson = tipPerson.toFixed(2);
         totalPerson = ((cost * percent) + cost) / number;
@@ -26,9 +74,9 @@ function tipCalculator(cost, percent, number) {
         totalSpan.innerHTML = "$" + totalPerson;
         console.log("Tip amount / person is " + tipPerson);
         console.log("Total / person is " + totalPerson);
-        reset.addEventListener('click', function (event) {
+        resetButton.addEventListener('click', function (event) {
             event.preventDefault();
-            reset.classList.add('tinted');
+            resetButton.classList.add('tinted');
             tipSpan.innerHTML = "$" + "0.00";
             totalSpan.innerHTML = "$" + "0.00";
             cost.value = 0;
@@ -41,60 +89,36 @@ function tipCalculator(cost, percent, number) {
     }
 }
 
-// makes all button elements toggle a background color.
-// Only one button can change at a time and when clicked.
-// If clicked anywhere else it does not disappear.
-const buttons = document.querySelectorAll('.btn-js');
-
-buttons.forEach(function (button) {
-    button.addEventListener('click', function (event) {
-        // Remove 'active' class from all buttons
-        event.preventDefault();
-        buttons.forEach(function (btn) {
-            btn.classList.remove('active');
-            event.preventDefault();
-        });
-
-        // Add 'active' class to the clicked button
-        button.classList.add('active');
-    });
-});
-
 // get numerical value of bill input
-const bill = document.querySelector('#bill');
-bill.addEventListener('input', function (event) {
-    // event.preventDefault();
+
+bill.addEventListener('input', function () {
     billValue = parseFloat(bill.value);
-    // console.log(billValue);
     tipCalculator(billValue, btnValue, peopleValue);
 });
 
 // get numerical value of button input
-const buttonsList = document.getElementsByClassName('btn-js');
+
 for (let i = 0; i < buttonsList.length; i++) {
     buttonsList[i].addEventListener('click', function (event) {
         event.preventDefault();
         if (buttonsList[i].classList.contains('active')) {
             btnValue = parseFloat(buttonsList[i].value);
-            // console.log(btnValue);
             tipCalculator(billValue, btnValue, peopleValue);
         }
     });
 }
 
-// get numerical value of custom input
-const custom = document.querySelector("#custom");
-custom.addEventListener('input', function () {
-    customValue = parseFloat(custom.value);
-    customValue = (customValue / 100);
-    // console.log(customValue);
-    tipCalculator(billValue, customValue, peopleValue);
-});
-
 // get numberical value of people input
 const people = document.querySelector("#people");
 people.addEventListener('input', function () {
     peopleValue = parseFloat(people.value);
-    // console.log(peopleValue);
     tipCalculator(billValue, btnValue, peopleValue);
+});
+
+// get numerical value of custom input
+
+customButton.addEventListener('input', function () {
+    customValue = parseFloat(customButton.value);
+    customValue = (customValue / 100);
+    tipCalculator(billValue, customValue, peopleValue);
 });
